1c9e3bb61e6992c5d7551ed9aa5c912bd257105e
FredB1 <fburke77@gmail.com>
2021-08-21 12:57:32 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+function eny() {
+  const start = new Date(2021, 08, 25, 6),
+    countDownDate = start.getTime(),
+    now = new Date().getTime(),
+    distance = countDownDate - now,
+    days = Math.floor(distance / (1000 * 60 * 60 * 24)),
+    hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+    seconds = Math.floor((distance % (1000 * 60)) / 1000)
+  return days + ":" + hours + ":" + minutes + ":" + seconds
+}
 function App() {
   return (
     <div className="App">
